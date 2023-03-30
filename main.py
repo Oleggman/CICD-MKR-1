9cdc4b239abd9458e873098a170b9a0566919735
Олег <olegkovalets1112003@knu.ua>
2023-03-30 11:00:31 +0300

    Sort by area

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ with open(file, 'r') as file:
 data = [line.strip() for line in data]
 data = [line.split(", ") for line in data]
 
-def get_sort(id):
-    return sorted(data, key=lambda x: int(x[id]))
+
+def sort_by_area():
+    print("Sort by area: ")
+    sorted_by_area = get_sort(1)
+    for country in sorted_by_area:
+        print(", ".join(country))
+
+
+def get_sort(el):
+    return sorted(data, key=lambda x: int(x[el]))
