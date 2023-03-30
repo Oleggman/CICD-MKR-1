341b5f226b6304e7a9b87c5f541862d3127ee563
Олег <olegkovalets1112003@knu.ua>
2023-03-30 11:44:32 +0300

    Test get sort

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -6,3 +6,10 @@ def test_read_info():
     assert read_info("test_file.txt") == [['Ukraine', '600000', '45000000']]
 
 
+def test_sort_data():
+    assert get_sort([['First element', '220', '430'],
+                     ['Second element', '105', '540'],
+                     ['Third element', '365', '445']], 1) == [['Second element', '105', '540'],
+                                                              ['First element', '220', '430'],
+                                                              ['Third element', '365', '445']]
+
