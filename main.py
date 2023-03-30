c15ffa7db9bcc9e14d8914f4bc16702140953301
Олег <olegkovalets1112003@knu.ua>
2023-03-30 11:23:55 +0300

    New sort by area

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def read_data(file):
+def read_info(file):
     with open(file, 'r') as f:
         data = [line.strip().split(',') for line in f]
     return data
 
+
+def sort_by_area(data):
+    sorted_data = sorted(data, key=lambda x: int(x[1]))
+    return sorted_data
 
