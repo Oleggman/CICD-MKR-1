dde83af0b9401e0d1a80695c886c0e40ab1fe954
Олег <olegkovalets1112003@knu.ua>
2023-03-30 11:22:44 +0300

    New file reader

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-file = "file.txt"
-
-with open(file, 'r') as file:
-    data = file.readline()
-
-data = [line.strip() for line in data]
-data = [line.split(", ") for line in data]
+def read_data(file):
+    with open(file, 'r') as f:
+        data = [line.strip().split(',') for line in f]
+    return data
 
 
-def sort_by_area():
-    print("Sort by area: ")
-    sorted_by_area = get_sort(1)
-    for country in sorted_by_area:
-        print(", ".join(country))
-
-
-def sort_by_population():
-    print("Sort by area: ")
-    sorted_by_population = get_sort(2)
-    for country in sorted_by_population:
-        print(", ".join(country))
-
-
-def get_sort(el):
-    return sorted(data, key=lambda x: int(x[el]))
